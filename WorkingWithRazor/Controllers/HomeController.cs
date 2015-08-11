8e73fafefd24381c6d1b2b9ab16414ff2ade6860
tonousa <tonousa@hotmail.com>
2015-08-11 14:12:26 -0400

    498

--- WorkingWithRazor/Controllers/HomeController.cs
+++ WorkingWithRazor/Controllers/HomeController.cs
@@ -21,5 +21,11 @@ namespace WorkingWithRazor.Controllers
         {
             return View();
         }
+
+        [ChildActionOnly]
+        public ActionResult Time(DateTime time)
+        {
+            return PartialView(time);
+        }
     }
 }
